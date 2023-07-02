e9c3cc7cf5c4f00c3423a670cf8b3e56815dea01
Manpreet Singh <manpreetmss400@gmail.com>
2023-07-02 17:29:59 +0530

    code structure improvements

--- road.js
+++ road.js
@@ -10,6 +10,16 @@ class Road{
     const infinity = 500; // 1000000 //->org value
     this.top = -infinity;
     this.bottom = infinity;
+    
+    const topLeft = {x:this.left, y: this.top};
+    const topRight ={x:this.right, y: this.top};
+    const bottomLeft = {x:this.left, y: this.bottom};
+    const bottomRight = {x:this.right, y: this.bottom};
+    
+    this.borders=[
+      [topLeft, bottomLeft],
+      [topRight , bottomRight],
+    ];
   }
   getLaneCenter(laneIndex){
     const laneWidth = this.width/this.laneCount;
@@ -22,7 +32,7 @@ class Road{
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
     
-    for(let i=0; i<=this.laneCount; i++){
+    for(let i=1; i<=this.laneCount-1; i++){
       const x = lerp(
         this.left,
         this.right,
@@ -30,16 +40,19 @@ class Road{
         
       );
       
-      if(i>0 && i<this.laneCount){
-        ctx.setLineDash([20, 20]);
-      }else{
-        ctx.setLineDash([]);  //means here [0,0]
-      }
-      
+      ctx.setLineDash([20, 20]);    //draw dashed lanes here
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+    
+    ctx.setLineDash([]);    // draw solid borders here
+    this.borders.forEach(border=>{  // traverse on border 1 by 1
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);  //1st point of border
+      ctx.lineTo(border[1].x, border[1].y);  //2nd point of border
+      ctx.stroke();
+    })
   }
 }
