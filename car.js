706459a33a3b086709dea80981bcce0f3d71be7f
Manpreet Singh <manpreetmss400@gmail.com>
2023-07-09 13:12:04 +0530

    multiple sd cars and detect best car

--- car.js
+++ car.js
@@ -138,7 +138,7 @@ class Car{
     this.y -= Math.cos(this.angle) * this.speed;
   }
     
-  draw(ctx, color){
+  draw(ctx, color, drawSensor=false){
     if(this.damaged){
       ctx.fillStyle = "gray";
     }else{
@@ -159,7 +159,7 @@ class Car{
 		
     ctx.fill();
     
-    if(this.sensors){
+    if(this.sensors && drawSensor){
       this.sensors.draw(ctx);
     }
   }
